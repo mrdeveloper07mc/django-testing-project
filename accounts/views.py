a549bd5d14e07c0388ffceeeefabbcf92eefcc29
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-03-06 14:38:23 +0500

    delete old project codes

--- accounts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
-
-# Create your views here.
-
-
-class LoginView(TemplateView):
-	template_name = 'layouts/login.html'
--- tester/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from .import views
-
-
-app_name = 'tester'
-
-urlpatterns = [
-	path('', views.index , name='index')
-]
