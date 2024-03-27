1e81f4078c5ac60971a32c9da5582d2ca932c3c0
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-03-27 14:51:21 +0500

    update

--- config/users/urls.py
+++ config/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView,LogoutView # standart login sahifasi views
+from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView # standart login sahifasi views
 
 from .import views
 
@@ -10,6 +10,10 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="auth/login.html"), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
     
-    path("register/",views.register, name='register')
+    path("register/",views.register, name='register'),
+    path("password_change/",PasswordChangeView.as_view(
+        template_name="auth/password_change_form.html",
+        success_url="/"
+        ), name='password_change'),
     
 ]
