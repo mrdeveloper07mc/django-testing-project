5538b087a25c5d7642a2294bd9153196ad295185
mrdeveloper07mc <165375051+mrdeveloper07mc@users.noreply.github.com>
2024-04-26 01:56:40 -0700

    https://github.com/kamilov2/django-tutorial/tree/main/kinobase

--- config/users/forms.py
+++ config/users/forms.py
@@ -7,3 +7,8 @@ class CustomUserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username","first_name","email","password1","password2"]
+
+        
+import telebot
+
+bot.send_message('-22222' ,message)
